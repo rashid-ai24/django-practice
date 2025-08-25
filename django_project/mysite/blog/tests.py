62085448286da47a49272616c84e61385f5375e3
MOHD_RASHID <moh.rashid20006@gmail.com>
2025-08-25 22:58:42 +0530

    Add tests for home and about page status codes

--- django_project/mysite/blog/tests.py
+++ django_project/mysite/blog/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class BlogTests(TestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get(reverse("blog-home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get(reverse("blog-about"))
+        self.assertEqual(response.status_code, 200)
