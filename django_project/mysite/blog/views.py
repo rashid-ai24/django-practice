284f6ac48b0a8d2635bbcd3910ac972c3520c524
raz-x <moh.rashid20006@gnail.com>
2025-08-25 22:30:54 +0530

    Temporary commit before normalizing line endings

---
+++ django_project/mysite/blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+# Create your views here.
+def home(request):
+    posts=Post.objects.all()
+    return render(request, "blog/home.html",{'posts':posts})
+def about(request):
+    return render(request, "blog/about.html")
